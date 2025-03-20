4c7f542253a96aeb5678742457ae434e195f5df2
Eya JLASSI_Codes <eya.jlassi@ensta-paris.fr>
2025-03-20 15:58:06 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="sql_query_and_plot_generator",  # Use a meaningful, lowercase name
     version="0.1.0",
     author="Wiem AMDOUNI","Mohamed Amine BESBES","Eya JLASSI","Yassine MKAOUAR"
-    author_email=amdouniwiem1@gmail.com","medaminbesbes91@gmail.com","eyajlassi2306@gmail.com","yassine.mkaouar15@gmail.com",
+    author_email="amdouniwiem1@gmail.com","medaminbesbes91@gmail.com","eyajlassi2306@gmail.com","yassine.mkaouar15@gmail.com",
     description="A Python package for SQL query generation and plots generation and visualization",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
