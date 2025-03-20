440cd1ce736f6d9ed485431268c9a992c449c67c
Eya JLASSI_Codes <eya.jlassi@ensta-paris.fr>
2025-03-20 16:01:28 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
+import codecs
 
 setup(
     name="sql_query_and_plot_generator",  # Use a meaningful, lowercase name
     version="0.1.0",
-    author="Wiem AMDOUNI","Mohamed Amine BESBES","Eya JLASSI","Yassine MKAOUAR"
-    author_email="amdouniwiem1@gmail.com","medaminbesbes91@gmail.com","eyajlassi2306@gmail.com","yassine.mkaouar15@gmail.com",
-    description="A Python package for SQL query generation and plots generation and visualization",
-    long_description=open("README.md").read(),
+    author="Wiem AMDOUNI, Mohamed Amine BESBES, Eya JLASSI, Yassine MKAOUAR",
+    author_email="amdouniwiem1@gmail.com, medaminbesbes91@gmail.com, eyajlassi2306@gmail.com, yassine.mkaouar15@gmail.com",
+    description="A Python package for SQL query generation and visualization",
+    long_description=codecs.open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/EyaJlassi695/LLM-for-Data-Analysis",  # Replace with your actual GitHub repo
     packages=find_packages(),
