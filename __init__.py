2089977c783a7b5e601265318c121d16291448b5
Eya JLASSI_Codes <eya.jlassi@ensta-paris.fr>
2025-03-20 16:04:30 +0100

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+"""
+sql_query_and_plot_generator package.
+
+This package provides SQL query generation, execution, and visualization tools.
+"""
+
 from .main import main
 from .utils import *  # Import everything from utils.py
 
